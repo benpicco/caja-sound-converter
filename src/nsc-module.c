d20267d8d818d78787f584aa5035de77492ebbde
bdpepple <bdpepple@280a3c95-c248-0410-a8b1-f7e71b7f0842>
2010-07-21 23:02:38 +0000

    Remove textdomain since this was causing translation issues with nautilus
    git-svn-id: https://nautilus-sound-converter.googlecode.com/svn/trunk@150 280a3c95-c248-0410-a8b1-f7e71b7f0842

--- src/nsc-module.c
+++ src/nsc-module.c
@@ -45,7 +45,6 @@ nautilus_module_initialize (GTypeModule *module)
 
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
 }
 
 void
